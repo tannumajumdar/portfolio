8903b10be4418e6f9ad072456319fcedb0ce172e
tannumajumdar <tannumajumdar88@gmail.com>
2026-07-29 16:12:16 +0530

    fix: resume

---
+++ index.js
@@ -0,0 +1,7 @@
+console.log("Hello, World!");
+
+setTimeout(() => {
+  console.log("This message is displayed after 2 seconds.");
+}, 2000);   
+
+console.log("This message is displayed immediately.");
